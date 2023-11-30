deff68ed08b07f16a6d6fb59382af1780f46a910
root <c.akunyiba@alustudent.com>
2023-11-30 13:38:13 +0000

    updated

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,11 +7,15 @@ import urllib.request
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode("utf-8")))
+	""" the method i used is easy way for string manipulation
+            to use a method that make use of the concept
+            print out html.decode('utf-8') with string formatting
+        """
 	temp = str(content)
 	print("\t- utf8 content: {}".format(temp[2:-1]))
